4089018e0a7060197c52302ab09f2a19d29d6f07
Francisco Gomes <95197081+fgsj0203@users.noreply.github.com>
2025-10-16 01:56:03 +0000

    feat: updating three implementation in three parts of chess game

--- xadrez.c
+++ xadrez.c
@@ -18,6 +18,12 @@ int main() {
    int i = 1;
    int j = 1;
    int k = 1;
+   int l = 1;
+   int m = 1;
+   int n = 1;
+   int o = 1;
+   int p = 1;
+   int q = 1;
     //-------------- End of section variables ----------------------
 
     // Implementation of looping for moving parts of chess / Implementando o looping para mover as peças do xadrez
@@ -45,7 +51,50 @@ int main() {
         printf("cima -> direita (%d - movimento)\n", k);
         k++;
     } while (k <= bispo);
+    //---------------------------------------------------------------
+    // Loop Structure for part - Tower / Estrutura de loop para a peça - Torre
+    printf("Loop For - part Tower / Loop for - peça torre\n");
+    for ( l = 1; l <= torre; l++)
+    {
+        printf("Direita -> (%d - movimento)\n", l);
+    }
+    // Using loop "While" for moving part - Tower / Usando loop "While" para mover a peça - Torre
+    printf("Loop While - part tower / Loop for - peça torre\n");
+    while (m <= torre)
+    {
+        printf("Direita -> (%d - movimento)\n", m);
+        m++;
+    }
+    // Loop structure "Do-While" part - Tower / Estrutura de loop para peça - Torre
+    printf("Loop Do-While - Part Tower / Loop Do-While - peça torre\n");
+    do
+    {
+        printf("Direita -> (%d - movimento)\n",n);
+        n++;
+    } while (n <= torre);
+    //-----------------------------------------------------------------------------------
+    // Looping structure for part - Queen / Estrutura de Loop para a peça - Rainha
+    printf("Loop for - part Queen / Estrutura For - peça Rainha\n");
+    for (o = 1; o <= rainha; o++)
+    {
+        printf("Rainha move <- (%d) para esquerda \n", o);
+    }
     
+    // Looping While for part - Quenn / Estrutura While para a peça - Rainha
+    printf("loop while - part Queen / Loop While - peça Rainha\n");
+    while (p <= rainha)
+    {
+        printf("Rainha move <- (%d) para esquerda \n", p);
+        p++;
+    }
+
+    // Looping Do-While for part - Queen / Estrutura Do-While para a peça - Rainha
+    printf("Loop Do-While - part Queen / Loop Do-While - peça Rainha\n");
+    do
+    {
+        printf("Rainha move <- (%d) para esquerda\n", q);
+        q++;
+    } while (q <= rainha);
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
